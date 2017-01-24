0a8efcae81b012eed72f4298b64aa34d79bd8871
BioMarek <BioMarek@users.noreply.github.com>
2017-01-24 13:41:45 +0100

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -3,6 +3,6 @@
 
 for file in *discarded_fasta
 do
-  # second sort sorts based on second column numerically
-  awk '{print length}' $file | sort | uniq -c | sort -n -k 2 > ${file:0:12}_counts.txt
+   # second sort sorts based on second column numerically
+   awk '{print length}' $file | sort | uniq -c | sort -n -k 2 > ${file:0:12}_counts.txt
 done
