ea86d759cfc1c321b0ca9b09105f7cd34ad2dce8
BioMarek <BioMarek@users.noreply.github.com>
2017-01-25 20:00:50 +0100

    Update discrading.sh

--- discrading.sh
+++ discrading.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
+mkdir /storage/brno7-cerit/home/marek_bfu/smRNA/discarded
 
 for file in *trimmed.fastq
 do
    # leaves sequences from fastq file and removes everything else
    sed -n 'n;p;n;n;' $file > ${file:0:12}_discarded_fasta.fasta
+   mv ${file:0:12}_discarded_fasta.fasta /storage/brno7-cerit/home/marek_bfu/smRNA/discarded
 done
 
