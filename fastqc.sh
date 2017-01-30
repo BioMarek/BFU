097f04058f6d99227414b494624315a26228d456
BioMarek <BioMarek@users.noreply.github.com>
2017-01-30 10:43:13 +0100

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 #
 mkdir /storage/brno7-cerit/home/marek_bfu/smRNA/fastqc_after_trim
+cd /storage/brno7-cerit/home/marek_bfu/smRNA/trimmed
 
 for file in *trimmed.fastq
 do
    cp *.fasta $SCRATCH
 done
 
+cd $SCRATCH
+module add fastQC-0.10.1
+
 for file in *trimmed.fastq
 do
-   fastqc $file
-   cp *.zip /storage/brno7-cerit/home/marek_bfu/smRNA/fastqc_after_trim
+   fastqc $file  
 done
 
+cp *.zip /storage/brno7-cerit/home/marek_bfu/smRNA/fastqc_after_trim
 rm -rf *
