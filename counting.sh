90b351923d6b46014ee4c94dc7783217d440b51b
BioMarek <BioMarek@users.noreply.github.com>
2017-01-30 10:50:16 +0100

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 mkdir /storage/brno7-cerit/home/marek_bfu/smRNA/counts
+cd /storage/brno7-cerit/home/marek_bfu/smRNA/discarded
 
 for file in *discarded_fasta.fasta
 do
@@ -8,6 +9,3 @@ do
    awk '{print length}' $file | sort | uniq -c | sort -n -k 2 > ${file:0:12}_counts.txt
    mv ${file:0:12}_counts.txt /storage/brno7-cerit/home/marek_bfu/smRNA/counts
 done
-
-
-
