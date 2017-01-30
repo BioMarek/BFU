e970e5c2475df34ec7e367132f985385792146b3
BioMarek <BioMarek@users.noreply.github.com>
2017-01-30 09:15:28 +0100

    Update cutadapt.sh

--- cutadapt.sh
+++ cutadapt.sh
@@ -16,8 +16,8 @@ for file in *.fastq
 do
   # cuts adapter and tosses everything shorter than 1 base
   cutadapt -a TGGAATTCTCGGGTGCCAAGG -m 1 -o ${file:0:12}_trimmed.fastq $file
+  mv ${file:0:12}_trimmed.fastq /storage/brno7-cerit/home/marek_bfu/smRNA/trimmed
 done
 
 mkdir /storage/brno7-cerit/home/marek_bfu/smRNA/trimmed
-mv * /storage/brno7-cerit/home/marek_bfu/smRNA/trimmed
 rm -rf *
