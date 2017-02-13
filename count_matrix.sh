c2987de3ef50a4cb9f1c23efe4e8fa7cb6211286
BioMarek <BioMarek@users.noreply.github.com>
2017-02-13 17:04:32 +0100

    Update count_matrix.sh

--- count_matrix.sh
+++ count_matrix.sh
@@ -1,5 +1,7 @@
 #! bin/bash/
 #
+# replace U with T
+cat Nicotiana_miRNA.fa  | tr U T > Nicotiana_miRNA_DNA.fa
 # -F interpret pattern as a list of fixed strings
 # -f load from file
-grep -F -f Nicotiana_miRNA.fa /home/marek/bfu/smRNA/H11_A_ATCACG_L006_R1_001.fastq
+grep -F -f Nicotiana_miRNA_DNA.fa /home/marek/bfu/smRNA/H11_A_ATCACG_L006_R1_001.fastq
