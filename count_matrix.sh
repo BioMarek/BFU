b5566375c8b6cb336578ed4568d2a34360907501
BioMarek <BioMarek@users.noreply.github.com>
2017-02-13 17:58:18 +0100

    Update count_matrix.sh

--- count_matrix.sh
+++ count_matrix.sh
@@ -5,7 +5,7 @@
 sed -n 'n;p;' Nicotiana_miRNA.fa | sort -u | tr U T > Nicotiana_miRNA_DNA.fa
 
 # sequencing files preparation
-sed -n 'n;p;n;n;' H11_A_ATCACG_L006_R1_001.fastq | sort | uniq -c > pok.fa
+sed -n 'n;p;n;n;' H11_A_ATCACG_L006_R1_001.fastq | sort | uniq -c | sort -n -r > pok.fa
 # -F interpret pattern as a list of fixed strings
 # -f load from fileq
 grep -F -f Nicotiana_miRNA_DNA.fa /home/marek/bfu/smRNA/H11_A_ATCACG_L006_R1_001.fastq
