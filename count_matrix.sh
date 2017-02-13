6ec46cc2e39f4afb2e32d19986f0a24456f3937c
BioMarek <BioMarek@users.noreply.github.com>
2017-02-13 17:35:45 +0100

    Update count_matrix.sh

--- count_matrix.sh
+++ count_matrix.sh
@@ -1,7 +1,8 @@
 #! bin/bash/
 #
-# tr replace U with T; sed removes annotations; sort removes duplicates
-cat Nicotiana_miRNA.fa  | tr U T | sed -n 'n;p;' | sort -u > Nicotiana_miRNA_DNA.fa
+# to get only unique DNA sequences of miRNA use following script 
+# sed removes annotations; sort removes duplicates; tr replace U with T
+sed -n 'n;p;' Nicotiana_miRNA.fa | sort -u | tr U T > Nicotiana_miRNA_DNA.fa
 # -F interpret pattern as a list of fixed strings
 # -f load from fileq
 grep -F -f Nicotiana_miRNA_DNA.fa /home/marek/bfu/smRNA/H11_A_ATCACG_L006_R1_001.fastq
