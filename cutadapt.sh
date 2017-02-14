2672764a1b4c550325de1064495cf225461c748a
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 08:49:26 +0100

    Update cutadapt.sh

--- cutadapt.sh
+++ cutadapt.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
+
+PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA
+DATASET_DIR=$PROJECT_DIR/raw_sequences # path to input raw sequences
+OUTPUT_DIR=$PROJECT_DIR/trimmed # path to output sequences
+
 module add python27-modules-gcc
 module add python27-modules-intel
 
-cd /storage/brno7-cerit/home/marek_bfu/smRNA/raw_sequences
+cd $DATASET_DIR
 
 for file in *.fastq
 do
@@ -11,13 +16,13 @@ do
 done
 
 cd $SCRATCH
-mkdir /storage/brno7-cerit/home/marek_bfu/smRNA/trimmed
+mkdir $OUTPUT_DIR
 
 for file in *.fastq
 do
   # cuts adapter and tosses everything shorter than 1 base
   cutadapt -a TGGAATTCTCGGGTGCCAAGG -m 1 -o ${file:0:12}_trimmed.fastq $file
-  mv ${file:0:12}_trimmed.fastq /storage/brno7-cerit/home/marek_bfu/smRNA/trimmed
+  mv ${file:0:12}_trimmed.fastq $OUTPUT_DIR
 done
 
 rm -rf *
