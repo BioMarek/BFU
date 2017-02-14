2de250e38ed5db7bb5a2e877cecc30a18b20a4cd
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 12:59:12 +0100

    Update count_matrix.sh

--- count_matrix.sh
+++ count_matrix.sh
@@ -29,3 +29,8 @@ done
 # -F interpret pattern as a list of fixed strings
 # -f load from fileq
 #grep -F -f Nicotiana_miRNA_DNA.fa /home/marek/bfu/smRNA/H11_A_ATCACG_L006_R1_001.fastq
+
+# second version
+# maybe use -c for counting
+# -w select only exact matches
+#grep -w '$sequence' $file
