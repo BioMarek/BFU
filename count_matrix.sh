57694ee577ba10a0b227ff7e901907729e9e01bd
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 12:26:28 +0100

    Update count_matrix.sh

--- count_matrix.sh
+++ count_matrix.sh
@@ -7,10 +7,14 @@ OUTPUT_DIR=$PROJECT_DIR/collapsed
 mkdir $OUTPUT_DIR
 cd $INPUT_DIR
 
+##################################################################################################
+##REFERENCE FILE MODIFICATION##
 # to get only unique DNA sequences of miRNA use following script 
 # sed removes annotations; sort removes duplicates; tr replace U with T
 #sed -n 'n;p;' Nicotiana_miRNA.fa | sort -u | tr U T > Nicotiana_miRNA_DNA.fa
 
+##################################################################################################
+##SEQUENCING FILES MODIFICATION##
 # sequencing files colapsing something like tally
 for file in *.fastq.gz
 do
@@ -20,6 +24,8 @@ do
   chmod 700 $OUTPUT_DIR/${file:0:12}.txt
 done
 
+##################################################################################################
+##MATRIX CREATION##
 # -F interpret pattern as a list of fixed strings
 # -f load from fileq
 #grep -F -f Nicotiana_miRNA_DNA.fa /home/marek/bfu/smRNA/H11_A_ATCACG_L006_R1_001.fastq
