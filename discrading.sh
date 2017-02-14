b29b4be109bee111b5e63cb6e8451f069a8c942e
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 08:38:25 +0100

    Update discrading.sh

--- discrading.sh
+++ discrading.sh
@@ -10,3 +10,13 @@ do
    mv ${file:0:12}_discarded_fasta.fasta /storage/brno7-cerit/home/marek_bfu/smRNA/discarded
 done
 
+mkdir /storage/brno7-cerit/home/marek_bfu/smRNA/counts
+cd /storage/brno7-cerit/home/marek_bfu/smRNA/discarded
+
+for file in *discarded_fasta.fasta
+do
+   # second sort sorts based on second column numerically
+   awk '{print length}' $file | sort | uniq -c | sort -n -k 2 > ${file:0:12}_counts.txt
+   mv ${file:0:12}_counts.txt /storage/brno7-cerit/home/marek_bfu/smRNA/counts
+done
+
