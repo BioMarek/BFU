68b8ccafd181eecda69b476c4441d3d8f76640cf
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 11:33:54 +0100

    Update count_matrix.sh

--- count_matrix.sh
+++ count_matrix.sh
@@ -12,10 +12,10 @@ cd $INPUT_DIR
 #sed -n 'n;p;' Nicotiana_miRNA.fa | sort -u | tr U T > Nicotiana_miRNA_DNA.fa
 
 # sequencing files colapsing something like tally
-for file in *.fastq
+for file in *.fastq.gz
 do
   # sed removes evrything but sequences; uniq counts how many sequences of each type there is, sort sorts numericaly in reverse order
-  sed -n 'n;p;n;n;' file | sort | uniq -c | sort -n -r > $OUTPUT_DIR/${file:0:12}.txt
+  gunzip -c $file | sed -n 'n;p;n;n;' file | sort | uniq -c | sort -n -r > $OUTPUT_DIR/${file:0:12}.txt
   # chmod sets access right so that only owner can work with file
   chmod 700 $OUTPUT_DIR/${file:0:12}.txt
 done
