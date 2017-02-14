a1a7d4fff66b9d0cf2d65d55d96422b0a718cb18
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 10:05:08 +0100

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -10,8 +10,10 @@ cd $DATASET_DIR
 
 for file in *trimmed.fastq
 do
-   # leaves sequences from fastq file and removes everything else
+   # leaves sequences from fastq file, removes everything else and saves result in temporary file
    sed -n 'n;p;n;n;' $file > ${file:0:12}_discarded_fasta.fasta
+   
+   # awk prints lengths of sequencs; uniq counts the sequences and number of couts; sort sorts numericaly based on second column
    awk '{print length}' ${file:0:12}_discarded_fasta.fasta | sort | uniq -c | sort -n -k 2 > ${file:0:12}_counts.txt
    mv ${file:0:12}_counts.txt $OUTPUT_DIR
    rm ${file:0:12}_discarded_fasta.fasta
