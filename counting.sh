10e83501e7927207ad615f3e86e3e96d97bfce72
BioMarek <BioMarek@users.noreply.github.com>
2017-02-14 09:58:45 +0100

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -14,4 +14,5 @@ do
    sed -n 'n;p;n;n;' $file > ${file:0:12}_discarded_fasta.fasta
    awk '{print length}' ${file:0:12}_discarded_fasta.fasta | sort | uniq -c | sort -n -k 2 > ${file:0:12}_counts.txt
    mv ${file:0:12}_counts.txt $OUTPUT_DIR
+   rm ${file:0:12}_discarded_fasta.fasta
 done
