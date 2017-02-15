1d95f1e5947132b8b9f724692e1746ffa14f462e
BioMarek <BioMarek@users.noreply.github.com>
2017-02-15 15:55:07 +0100

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,14 +1,23 @@
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+# The scritpt opens file with miRNAs sequences and removes duplicetes. The first occurence of each  sequence and its 
+# respective name is kept. This is because of inconsistent naming of plant  Nicotiana tabacum miRNAs - there is several 
+# miRNAs with same sequence but  different names.
+
+#######################################################################################################################
+###SCRIPT BODY###
 from Bio import SeqIO
 
-f_result = open("Nicotiana_miRNA_D_no_duplicates.fa", 'w')
+f_result = open("Nicotiana_miRNA_D_no_duplicates.fa", 'w')  # file to witch we copy filtered sequences 
+sequences = []  # temporary list used to store sequences already written into f_result file
 
-sequences = []
 for seq_record in SeqIO.parse("Nicotiana_miRNA_D.fa", "fasta"):
-    print(seq_record.seq)
+    # print(seq_record.seq)  # just for our information
     if seq_record.seq not in sequences:
         sequences.append(seq_record.seq)
-        final_result = (">" seq_record.description + '\n' + seq_record.seq + '\n')
+        # for some reason description is parsed without '>' character therefore it must be added
+        final_result = '>' + seq_record.description + '\n' + seq_record.seq + '\n'
         f_result.write(str(final_result))
 
-print(len(sequences))
+print(len(sequences)) # number of sequences that passed the filter
 f_result.close()
