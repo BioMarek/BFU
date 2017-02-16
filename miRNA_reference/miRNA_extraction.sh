b5b936b40c6ad33fb87871ad29788c85a010cabe
BioMarek <BioMarek@users.noreply.github.com>
2017-02-16 19:41:24 +0100

    Update miRNA_extraction.sh

--- miRNA_reference/miRNA_extraction.sh
+++ miRNA_reference/miRNA_extraction.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+#
+##############################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
 # Used to extract miRNA belonging to specific species from miRBase fasta file
 
-# sed version
+##############################################################################################################################
+##SPECIFY DATA VARIABLES###
+SPECIES_ID=">nta" # !!!test this!!!
+
+##############################################################################################################################
+###SCRIPT BODY###
 # >nta is unique identificator for Nicotiana tabacum miRNAs
-sed -n '/>nta/{p;n;p}' mature.fa > Nicotiana_miRNA.fa
+sed -n '/$SPECIES_ID/{p;n;p}' mature.fa > Nicotiana_miRNA.fa
