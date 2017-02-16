867ae9f8bea1dec7403acef73bedbda53cab8300
BioMarek <BioMarek@users.noreply.github.com>
2017-02-16 19:37:02 +0100

    Update 3.2_counting.sh

--- 3.2_counting.sh
+++ 3.2_counting.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+# The script counts lengths of sequences so we can make histogram
 
+#######################################################################################################################
+##SPECIFY DATA VARIABLES###
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA
 DATASET_DIR=$PROJECT_DIR/filtered # path to input raw sequences
 OUTPUT_DIR=$PROJECT_DIR/counts # path to output sequences
 
+#######################################################################################################################
+###SCRIPT BODY###
 mkdir $OUTPUT_DIR
 cd $DATASET_DIR
 
