042c939556c31bedb948f163338ce313d88da95b
BioMarek <BioMarek@users.noreply.github.com>
2017-02-17 19:05:13 +0100

    Update 3.2_counting.sh

--- 3.2_counting.sh
+++ 3.2_counting.sh
@@ -18,7 +18,7 @@ cd $DATASET_DIR
 for file in *mirna.fastq.gz
 do
    # leaves sequences from fastq file, removes everything else, awk prints lengths of sequencs; uniq counts the sequences.
-   gunzip -c $file | sed -n 'n;p;n;n;' | awk '{print length}' $file | sort | uniq -c > $OUTPUT_DIR/${file:0:12}_counts.txt
+   gunzip -c $file | sed -n 'n;p;n;n;' | awk '{print length}' | sort | uniq -c > $OUTPUT_DIR/${file:0:12}_counts.txt
    # chmod sets access right so that only owner can work with file
    chmod 700 $OUTPUT_DIR/${file:0:12}_counts.txt
 done
