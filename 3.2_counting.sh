ce754d1f0e0ac5b5f407b13bf63bdc47d46a5a6c
BioMarek <BioMarek@users.noreply.github.com>
2017-02-17 19:02:41 +0100

    Update 3.2_counting.sh

--- 3.2_counting.sh
+++ 3.2_counting.sh
@@ -1,5 +1,3 @@
-# IT'S FUCKED UP! REPAIR IT!!!
-
 #!/bin/bash
 #
 #######################################################################################################################
@@ -19,16 +17,8 @@ cd $DATASET_DIR
 
 for file in *mirna.fastq.gz
 do
-   # leaves sequences from fastq file, removes everything else and saves result in temporary file
-   gunzip -c $file | sed -n 'n;p;n;n;' > ${file:0:12}_discarded_fasta.fasta
-   
-done
-
-for file in *_discarded_fasta.fasta
-do
-   # awk prints lengths of sequencs; uniq counts the sequences and number of couts
-   awk '{print length}' $file | sort | uniq -c > $OUTPUT_DIR/${file:0:12}_counts.txt
+   # leaves sequences from fastq file, removes everything else, awk prints lengths of sequencs; uniq counts the sequences.
+   gunzip -c $file | sed -n 'n;p;n;n;' | awk '{print length}' $file | sort | uniq -c > $OUTPUT_DIR/${file:0:12}_counts.txt
    # chmod sets access right so that only owner can work with file
    chmod 700 $OUTPUT_DIR/${file:0:12}_counts.txt
-   rm ${file:0:12}_discarded_fasta.fasta
 done
