c8db4f7979a7c1afcb9992600281b66aa72a27c9
BioMarek <BioMarek@users.noreply.github.com>
2017-02-17 08:27:08 +0100

    Update 3.2_counting.sh

--- 3.2_counting.sh
+++ 3.2_counting.sh
@@ -15,10 +15,10 @@ OUTPUT_DIR=$PROJECT_DIR/counts # path to output sequences
 mkdir $OUTPUT_DIR
 cd $DATASET_DIR
 
-for file in *trimmed.fastq
+for file in *mirna.fastq.gz
 do
    # leaves sequences from fastq file, removes everything else and saves result in temporary file
-   sed -n 'n;p;n;n;' $file > ${file:0:12}_discarded_fasta.fasta
+   gunzip -c $file | sed -n 'n;p;n;n;' > ${file:0:12}_discarded_fasta.fasta
    
    # awk prints lengths of sequencs; uniq counts the sequences and number of couts; sort sorts numericaly based on second column
    awk '{print length}' ${file:0:12}_discarded_fasta.fasta | sort | uniq -c | sort -n -k 2 > $OUTPUT_DIR/${file:0:12}_counts.txt
