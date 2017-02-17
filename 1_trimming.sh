2b0f9cfe6742e0db05bb5fa1679b3c1ef3f3106a
BioMarek <BioMarek@users.noreply.github.com>
2017-02-17 22:15:34 +0100

    Update 1_trimming.sh

--- 1_trimming.sh
+++ 1_trimming.sh
@@ -2,14 +2,14 @@
 #
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
-# The script trips adapter sequence from sequencing data
+# The script trims adapter sequences using cutadapt from raw sequencing data
 
 #######################################################################################################################
 ##SPECIFY DATA VARIABLES###
-PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA
+PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 DATASET_DIR=$PROJECT_DIR/raw_sequences # path to input raw sequences
 OUTPUT_DIR=$PROJECT_DIR/trimmed # path to output sequences
-ADAPTER="TGGAATTCTCGGGTGCCAAGG" # adapter sequence to be trimmed
+ADAPTER="TGGAATTCTCGGGTGCCAAGG" # adapter sequence to be trimmed (illumina adapter RNA 3’ Adapter (RA3))
 
 #######################################################################################################################
 ###SCRIPT BODY###
@@ -30,6 +30,7 @@ for file in *.fastq.gz
 do
   # cuts adapter and tosses everything shorter than 1 base
   cutadapt -a $ADAPTER -m 1 -o ${file:0:12}_trimmed.fastq.gz $file
+  # when working in $SCRATCH I do calculation first and then move files with results in order to take advantage of SSDs
   mv ${file:0:12}_trimmed.fastq.gz $OUTPUT_DIR
 done
 
