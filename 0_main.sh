2e7656d87e0021d5cbbc4164c99f6167170cf800
BioMarek <BioMarek@users.noreply.github.com>
2017-02-17 09:01:38 +0100

    Update 0_main.sh

--- 0_main.sh
+++ 0_main.sh
@@ -3,7 +3,7 @@
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
 # This script runs the pipeline
-# Start with: qsub -l walltime=12h -l mem=2gb -l scratch=100gb -l nodes=1:ppn=2 0_main.sh
+# Start with: qsub -l walltime=12h -l mem=1gb -l scratch=40gb -l nodes=1:ppn=2 0_main.sh
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA/
 cd $PROJECT_DIR
 
