a016f9707d50772ab5042f26a4eb254e1863250d
BioMarek <BioMarek@users.noreply.github.com>
2017-03-05 09:08:01 +0100

    Update 3_collapsing.sh

--- 3_collapsing.sh
+++ 3_collapsing.sh
@@ -2,6 +2,8 @@
 #
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
+## SCRIPT INPUT  *.fastq.gz filtered sequences with proper length and high quality
+## SCRIPT OUTPUT *.txt sequences where in the first column there is number of sequences which are in the second column
 # The script collapses identical sequeces to sinlge one while keeping information about number of reads, something
 # like Tally http://wwwdev.ebi.ac.uk/enright-dev/kraken/reaper/src/reaper-latest/doc/tally.html
 # sequences with only one hit are discarded
@@ -17,10 +19,11 @@ cd $INPUT_DIR
 ###SCRIPT BODY###
 for file in *mirna.fastq.gz
 do
-  # gunzip -c unpacks wihout modifying the input file and sends result to STDOUT; sed removes evrything but sequences; 
-  # uniq counts how many sequences of each type there is (first sort is there because uniq requires adjacent matching lines); 
-  # second sort sorts numericaly in reverse order and grep removes sequences with sigle hit (multiple spaces than number 1 
-  # and then space)
+  # gunzip -c unpacks wihout modifying the input file and sends result to STDOUT
+  # sed removes evrything but sequences
+  # uniq counts how many sequences of each type there is (first sort is there because uniq requires adjacent matching lines) 
+  # second sort sorts numericaly in reverse order
+  # grep removes sequences with sigle hit (multiple spaces than number 1 and then space)
   gunzip -c $file | sed -n 'n;p;n;n;' | sort | uniq -c | sort -n -r | grep -v '^[ ]*1 ' > $OUTPUT_DIR/${file:0:12}.txt
   # chmod sets access right so that only owner can work with file
   chmod 700 $OUTPUT_DIR/${file:0:12}.txt
