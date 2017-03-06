50ecc3ed079ab686872065017020d56aa508fa3b
BioMarek <BioMarek@users.noreply.github.com>
2017-03-06 10:06:44 +0100

    Update 1_sequence_extraction_Rfam.sh

--- Rfam_database_mining/1_sequence_extraction_Rfam.sh
+++ Rfam_database_mining/1_sequence_extraction_Rfam.sh
@@ -6,7 +6,7 @@
 
 ##############################################################################################################################
 ##SPECIFY DATA VARIABLES###
-SPECIES_ID="Nicotiana tabacum" # unique identificator for Nicotiana tabacum miRNAs
+SPECIES_ID="Nicotiana tabacum" # unique identificator for Nicotiana tabacum ncRNAs
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 INPUT_DIR=$PROJECT_DIR/rfam/12.2/fasta_files # path to input filtered sequences
 OUTPUT_DIR=$PROJECT_DIR/reference # path to output sequences
@@ -17,7 +17,9 @@ cd $INPUT_DIR
 
 for file in *.fa.gz
 do
-  gunzip -c $file | sed -n '/$SPECIES_ID/{p;n;p}' >> $OUTPUT_DIR/Nicotiana_Rfam_seq.fa
+  # double quotes because of some stupid subtitution behaviour 
+  # See: http://stackoverflow.com/questions/584894/sed-scripting-environment-variable-substitution
+  gunzip -c $file | sed -n "/$SPECIES_ID/{p;n;p}" >> $OUTPUT_DIR/Nicotiana_Rfam_seq.fa
 done
 
 chmod 700 $OUTPUT_DIR/Nicotiana_Rfam_seq.fa
