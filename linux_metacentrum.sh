b8c1f204c1fdcbe0ad2eef6f84fac928d90aae87
BioMarek <BioMarek@users.noreply.github.com>
2017-03-09 20:53:02 +0100

    Create linux_metacentrum.sh

---
+++ linux_metacentrum.sh
@@ -0,0 +1,8 @@
+# connecting to machine
+ssh marek_bfu@onyx.ncbr.muni.cz
+
+# copying to the metacentrum
+scp $file marek_bfu@onyx.ncbr.muni.cz:
+
+# copying from metacentrum
+scp marek_bfu@onyx.ncbr.muni.cz:$file $DESTINATION_DIR
