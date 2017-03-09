84c8dbde4953ef1c17072f078016af4f1cb66cc3
BioMarek <BioMarek@users.noreply.github.com>
2017-03-09 10:17:11 +0100

    Update and rename 2_fastq_to_fasta to 2_fastq_to_fasta.sh

--- Rfam_database_mining/2_fastq_to_fasta.sh
+++ Rfam_database_mining/2_fastq_to_fasta.sh
@@ -2,7 +2,7 @@
 #
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
-## SCRIPT INPUT  *.fastq file
+## SCRIPT INPUT  *.fastq.gz file
 ## SCRIPT OUTPUT *.fasta file
 # The script converts *.fastq file to *.fasta file so that metacntrum blast can use it
 
@@ -11,7 +11,11 @@
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 INPUT_DIR=$PROJECT_DIR/reference # path to input filtered sequences
 OUTPUT_DIR=$PROJECT_DIR/reference # path to output sequences
+INPUT_FILE=file.fq.gz
+OUTPUT_FILE=file.fa
 
 #######################################################################################################################
 ###SCRIPT BODY###
-gunzip -c file.fq.gz | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > file.fa
+cd $INPUT_DIR
+
+gunzip -c $INPUT_FILE | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > $OUTPUT_DIR/$OUTPUT_FILE
