47761a47c51a5f284a85d147136d2edb8e6966bf
BioMarek <BioMarek@users.noreply.github.com>
2017-03-13 17:19:29 +0100

    Update blasting.sh

--- Rfam_database_mining/blasting.sh
+++ Rfam_database_mining/blasting.sh
@@ -11,7 +11,6 @@
 ###SPECIFY DATA VARIABLES###
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 INPUT_DIR=$PROJECT_DIR/blast # path to input sequences
-REFERENCE_FILE=$PROJECT_DIR/reference/Nicotiana_Rfam_seq.fa
 OUTPUT_DIR=$PROJECT_DIR/blasted # path to output sequences
 
 #######################################################################################################################
@@ -20,8 +19,22 @@ mkdir $OUTPUT_DIR
 cd $INPUT_DIR
 
 cp * $SCRATCH
+cp $REFERENCE_DIR/$REFERENCE_FILE $SCRATCH
 
 ###CREATION OF REFERENCE FILES###
+while read line
+do
+  count=$[count + 1]
+  if [ ${line:0:1} == '>' ] # if first line is description create new file 
+  then
+    touch Nicotiana_Rfam_seq_"$count"ref.fa
+    temp=Nicotiana_Rfam_seq_"$count"ref.fa # filename must be stored for the next cycle in which we save the sequence into said file
+    echo $line >> Nicotiana_Rfam_seq_"$count"ref.fa
+  else
+    # double quotes gets rid of "ambiguous redirect" error http://stackoverflow.com/questions/2462385/getting-an-ambiguous-redirect-error 
+    echo $line >> "$temp"
+  fi
+done < Nicotiana_Rfam_seq.fa
 
 
 
