6b671bd853a1dcc251be15fd3d4a2060d6bfec2a
BioMarek <BioMarek@users.noreply.github.com>
2017-03-13 18:08:28 +0100

    Update file_from_single_sequence.sh

--- Rfam_database_mining/file_from_single_sequence.sh
+++ Rfam_database_mining/file_from_single_sequence.sh
@@ -15,17 +15,17 @@ OUTPUT_DIR=$PROJECT_DIR/test_blast # path to output sequences
 ###SCRIPT BODY###
 mkdir $OUTPUT_DIR
 cd $INPUT_DIR
-count=0
+count=1
 
 # rework for 2 lines, if line starts with > create file and copy line into it else copy line into previous file
 while read line
 do
-  count=$[count + 1]
   if [ ${line:0:1} == '>' ] # if first line is description create new file 
   then
     touch "$OUTPUT_DIR"/Nicotiana_Rfam_seq_"$count"ref.fa
     temp="$OUTPUT_DIR"/Nicotiana_Rfam_seq_"$count"ref.fa # filename must be stored for the next cycle in which we save the sequence into said file
     echo $line >> $OUTPUT_DIR/Nicotiana_Rfam_seq_"$count"ref.fa
+    count=$[count + 1]
   else
     # double quotes gets rid of "ambiguous redirect" error http://stackoverflow.com/questions/2462385/getting-an-ambiguous-redirect-error 
     echo $line >> "$temp"
