c1195e9d5449f2d350232f66bd9a30bd707a9d4e
BioMarek <BioMarek@users.noreply.github.com>
2017-03-13 16:46:33 +0100

    Update 2.1_fasta_from_collapsed.sh

--- Rfam_database_mining/2.1_fasta_from_collapsed.sh
+++ Rfam_database_mining/2.1_fasta_from_collapsed.sh
@@ -1,21 +1,26 @@
 #! /bin/bash
 #
-##############################################################################################################################
+#######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
-# The script creates *.fa file from collapsed files so that metacentrum blastn can use them
+## SCRIPT INPUT  *.txt contining collapsed sequences
+## SCRIPT OUTPUT *.fa file containing collapsed files
+# The script creates *.fa file from collapsed files so that metacentrum blastn can use them. In the decription is 
+# number of each sequence followed by nucleotide sequence
 
-##############################################################################################################################
+
+#######################################################################################################################
 ##SPECIFY DATA VARIABLES###
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 INPUT_DIR=$PROJECT_DIR/collapsed # path to input filtered sequences
 OUTPUT_DIR=$PROJECT_DIR/blast # path to output sequences
 
 #######################################################################################################################
+
 ###SCRIPT BODY###
 cd $INPUT_DIR
 
 for file in *.txt
 do
   # in the description there is sequence as a "name" and number of these sequences delimited by '|' character
-  awk '{print ">" $2 "|" $1 "\n" $2}' $file > $OUTPUT_DIR/${file:0:12}_collapsed.fa
+  awk '{print ">" $1 " " $2 "\n" $2}' $file > $OUTPUT_DIR/${file:0:12}_collapsed.fa
 done
