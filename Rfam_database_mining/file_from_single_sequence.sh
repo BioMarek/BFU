90b6e7814e5ecba613ab6cf50a42cd792bdbf446
BioMarek <BioMarek@users.noreply.github.com>
2017-03-13 10:28:50 +0100

    Update file_from_single_sequence.sh

--- Rfam_database_mining/file_from_single_sequence.sh
+++ Rfam_database_mining/file_from_single_sequence.sh
@@ -21,13 +21,13 @@ count=0
 while read line
 do
   count=$[count + 1]
-  if [ ${line:0:1} == '>' ] # if first line is description create name with said name TODO spaces?
+  if [ ${line:0:1} == '>' ] # if first line is description create new file 
   then
     touch "$OUTPUT_DIR"/Nicotiana_Rfam_seq_"$count".fa
     temp="$OUTPUT_DIR"/Nicotiana_Rfam_seq_"$count".fa # filename must be stored for the next cycle in which we save the sequence into said file
     echo $line >> $OUTPUT_DIR/Nicotiana_Rfam_seq_"$count".fa
   else
-    # quotes gets rid of "ambiguous redirect" error http://stackoverflow.com/questions/2462385/getting-an-ambiguous-redirect-error 
+    # double quotes gets rid of "ambiguous redirect" error http://stackoverflow.com/questions/2462385/getting-an-ambiguous-redirect-error 
     echo $line >> "$temp"
   fi
 done < Nicotiana_Rfam_seq.fa
