57ab4ed07afa07b9b75a1bc2d96d767847866eaf
BioMarek <BioMarek@users.noreply.github.com>
2017-03-15 18:53:44 +0100

    Update blasting.sh

--- Rfam_database_mining/blasting.sh
+++ Rfam_database_mining/blasting.sh
@@ -2,6 +2,7 @@
 #
 ##############################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
+## run with qsub -l walltime=2d -l mem=100mb -l scratch=40gb:ssd -l nodes=1:ppn=1
 ## SCRIPT INPUT  *.fa sequences of collapsed files
 ## SCRIPT OUTPUT *.txt with number of how many sequences was mapped
 # The script blasts two files. If there is match it takes number of sequence that has been matched and adds it to variable 
@@ -12,6 +13,7 @@
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 INPUT_DIR=$PROJECT_DIR/blast # path to input sequences
 OUTPUT_DIR=$PROJECT_DIR/blasted # path to output sequences
+FILE=H11_A_ATCACG # file which is being calculated
 
 #######################################################################################################################
 ###SCRIPT BODY###
@@ -46,30 +48,33 @@ while read line
 do
   if [ ${line:0:1} == '>' ] # if first line is description create new file
   then
-    touch H11_A_ATCACG_"$count"_que.fa
-    temp=H11_A_ATCACG_"$count"_que.fa # filename must be stored for the next cycle in which we save the sequence into said file
-    echo $line >> H11_A_ATCACG_"$count"_que.fa
+    touch "$FILE"_"$count"_que.fa
+    temp="$FILE"_"$count"_que.fa # filename must be stored for the next cycle in which we save the sequence into said file
+    echo $line >> "$FILE"_"$count"_que.fa
     count=$[count + 1]
   else
     # double quotes gets rid of "ambiguous redirect" error http://stackoverflow.com/questions/2462385/getting-an-ambiguous-redirect-error 
     echo $line >> "$temp"
   fi
-done < H11_A_ATCACG_collapsed.fa
+  # first 50000 most numerous sequences this should take 40h to complete
+done < head -n100000 | "$FILE"_collapsed.fa
 
 
 ###BLASTING###
 for SUBJECT in *ref.fa
 do
   touch "$SUBJECT"_hits.txt
+  cat "$SUBJECT" >> "$SUBJECT"_hits.txt # adds name and nucleotide sequence to the result file
 
   for QUERY in *que.fa
   do
     blastn -query $QUERY -subject $SUBJECT > result
     # -q tells grep to retun only exit status; 0 if somethig was found; different number otherwise
-    if grep -q "No hits found" result
+    # -v is reverse search
+    if grep -q -v "No hits found" result
     then
-      rm result
-    else
+      # -o print only the matched (non-empty) parts of matching lines
+      # -P interpret the pattern as a Perl-compatible regular expression (PCRE)
       grep -o -P '(?<=Query= ).*(?= )' result >> "$SUBJECT"_hits.txt # saves number of sequences that had been succesfully matched to res file
     fi
   done
