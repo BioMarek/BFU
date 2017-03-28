baded310707151d15568ea9339d51aa1a7071a13
BioMarek <BioMarek@users.noreply.github.com>
2017-03-28 13:07:31 +0200

    Update bowtieing.sh

--- Rfam_database_mining/bowtieing.sh
+++ Rfam_database_mining/bowtieing.sh
@@ -20,41 +20,15 @@ mkdir $OUTPUT_DIR
 cd $INPUT_DIR
 
 module add bowtie-0.12.8
+bowtie
 
-cp * $SCRATCH
-cd $SCRATCH
+# translate spaces to underscores because the name of sequence is truncated after first space
+tr ' ' '_' < Nicotiana_Rfam_seq.fa > Nicotiana_Rfam_seq_tr.fa
+# create index
+bowtie-build Nicotiana_Rfam_seq_tr.fa Nicotiana_Rfam
 
-#######################################################################################################################
-###CREATING SINLGE FILES FROM REFERENCE###
-## translate spaces to underscores because the name of sequence is truncated after first space
-#tr ' ' '_' < Nicotiana_Rfam_seq.fa > Nicotiana_Rfam_seq_tr.fa
-# rework for 2 lines, if line starts with > create file and copy line into it else copy line into previous file
-while read line
+# align
+for file in *collapsed.fa
 do
-  if [ ${line:0:1} == '>' ] # if first line is description create new file 
-  then
-    touch "$line"_ref.fa
-    temp="$line"_ref.fa # filename must be stored for the next cycle in which we save the sequence into said file
-    echo $line >> "$line"_ref.fa
-  else
-    # double quotes gets rid of "ambiguous redirect" error http://stackoverflow.com/questions/2462385/getting-an-ambiguous-redirect-error 
-    echo $line >> "$temp"
-  fi
-done < Nicotiana_Rfam_seq.fa
-
-
-#######################################################################################################################
-###BOWTIE ALIGNMENT###
-
-for reference in *ref.fa 
-do
-  # for each file creates reference index, the names are always same so they are rewritten
-  bowtie-build $reference Nicotiana_Rfam
-  # align
-  for file in *collapsed.fa
-  do
-    bowtie -f Nicotiana_Rfam $file | sort -t: -k2 > $OUTPUT_DIR/"$reference"_${file:0:5}_aligned.bow
-  done
+  bowtie -f Nicotiana_Rfam $file | sort -t: -k2 > $OUTPUT_DIR/${file:0:12}_aligned.bow
 done
-
-rm *
