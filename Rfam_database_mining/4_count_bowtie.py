6edc5d0203c4899a543ba61ac5a329887c30cd7c
BioMarek <BioMarek@users.noreply.github.com>
2017-03-29 11:33:22 +0200

    Update 4_count_bowtie.py

--- Rfam_database_mining/4_count_bowtie.py
+++ Rfam_database_mining/4_count_bowtie.py
@@ -13,13 +13,18 @@ for processed_file in processed_list:
     df = pandas.read_csv(input, delimiter='\t')
 
     for i in range(len(df)):
-        if df.iloc[i, 2] not in sequences:
+        # deals with problem with first zero
+        if i == 0:
             sequences.append(df.iloc[i, 2])
-            #result_to_write = str(df.iloc[i, 2]) + ' ' + str(count) + '\n'
-            result_to_write = str(count) + '\n'
-            f_result.write(result_to_write)
-            count = 0
-        else:
             count += int(df.iloc[i, 0])
+        else:
+            if df.iloc[i, 2] not in sequences:
+                sequences.append(df.iloc[i, 2])
+                #result_to_write = str(df.iloc[i, 2]) + ' ' + str(count) + '\n'
+                result_to_write = str(count) + '\n'
+                f_result.write(result_to_write)
+                count = 0
+            else:
+                count += int(df.iloc[i, 0])
 
     f_result.close()
