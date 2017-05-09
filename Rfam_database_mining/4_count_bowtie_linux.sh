7104d30d6ae5b0e93e1ad6b912400b4690a2a79a
BioMarek <BioMarek@users.noreply.github.com>
2017-05-09 09:47:46 +0200

    Update 4_count_bowtie_linux.sh

--- Rfam_database_mining/4_count_bowtie_linux.sh
+++ Rfam_database_mining/4_count_bowtie_linux.sh
@@ -3,14 +3,14 @@
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
 ## SCRIPT INPUT  *.bow files from bowtie
-## SCRIPT OUTPUT 
-# The script runs python script to get counts
+## SCRIPT OUTPUT *.counts matrix file containing counts from all files
+# The script runs python script to get counts and creates matrix for further analyses
 
 #######################################################################################################################
 ###SPECIFY DATA VARIABLES###
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
 INPUT_DIR=$PROJECT_DIR/aligned # path to input sequences and reference
-OUTPUT_DIR=$PROJECT_DIR/bow_counts
+OUTPUT_DIR=$PROJECT_DIR/bow_counts # path to output sequences
 
 #######################################################################################################################
 ###SCRIPT BODY###
@@ -26,6 +26,9 @@ python $PROJECT_DIR/scripts/4_count_bowtie.py
 mv *.counts $OUTPUT_DIR
 cd $OUTPUT_DIR
 
+# creates matrix file
 paste H11_A_ATCACG_aligned.counts H11_B_CGATGT_aligned.counts P1_A_CAGATC__aligned.counts P1_B_ACTTGA__aligned.counts \
       P3_A_GATCAG__aligned.counts P3_B_TAGCTT__aligned.counts P8_A_TTAGGC__aligned.counts P8_B_TGACCA__aligned.counts \
       REG_A_ACAGTG_aligned.counts REG_B_GCCAAT_aligned.counts > ncRNA_matrix_count.counts
+      
+rm *.aligned.counts
