c7fad132b164b9961349b469f2d74863cae973bb
BioMarek <BioMarek@users.noreply.github.com>
2017-05-09 11:03:32 +0200

    Update 4_count_bowtie_linux.sh

--- Rfam_database_mining/4_count_bowtie_linux.sh
+++ Rfam_database_mining/4_count_bowtie_linux.sh
@@ -26,9 +26,13 @@ python $PROJECT_DIR/scripts/4_count_bowtie.py
 mv *.counts $OUTPUT_DIR
 cd $OUTPUT_DIR
 
+# creates matrix file with header
+touch ncRNA_matrix_count.counts
+echo name\t H11_A \tH11_B \tP1_A \tP1_B \tP3_A \tP3_B \tP8_A \tP8_B \tREG_A \tREG_B > ncRNA_matrix_count.counts
+
 # creates matrix file
 paste H11_A_ATCACG_aligned.counts H11_B_CGATGT_aligned.counts P1_A_CAGATC__aligned.counts P1_B_ACTTGA__aligned.counts \
       P3_A_GATCAG__aligned.counts P3_B_TAGCTT__aligned.counts P8_A_TTAGGC__aligned.counts P8_B_TGACCA__aligned.counts \
-      REG_A_ACAGTG_aligned.counts REG_B_GCCAAT_aligned.counts > ncRNA_matrix_count.counts
+      REG_A_ACAGTG_aligned.counts REG_B_GCCAAT_aligned.counts >> ncRNA_matrix_count.counts
       
 rm *aligned.counts
