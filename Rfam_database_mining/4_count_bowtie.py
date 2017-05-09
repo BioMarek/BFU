3a70cf05bd3d2f54a53629c430b738653c62d62e
BioMarek <BioMarek@users.noreply.github.com>
2017-05-09 10:03:41 +0200

    Update 4_count_bowtie.py

--- Rfam_database_mining/4_count_bowtie.py
+++ Rfam_database_mining/4_count_bowtie.py
@@ -1,9 +1,17 @@
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+## SCRIPT INPUT  *.bow files from bowtie
+## SCRIPT OUTPUT *.counts files
+# The script takes alignment files from bowtie and creates counts for each file. These files are in subsequent step 
+# merged to create count matrix.
+
+#######################################################################################################################
+###SCRIPT BODY###
 import pandas
 
 processed_list = ['H11_A_ATCACG', 'H11_B_CGATGT','P1_A_CAGATC_', 'P1_B_ACTTGA_', 'P3_A_GATCAG_', 'P3_B_TAGCTT_',
                   'P8_A_TTAGGC_', 'P8_B_TGACCA_', 'REG_A_ACAGTG', 'REG_B_GCCAAT']
 
-#processed_list = ['H11_A_ATCACG'] # switch comments to get first column wit names of sequences
 for processed_file in processed_list:
     f_result = open(processed_file + '_aligned.counts', 'w')
     sequences = []
@@ -22,7 +30,7 @@ for processed_file in processed_list:
         else:
             if df.iloc[i, 2] not in sequences:
                 sequences.append(df.iloc[i, 2])
-                if processed_file == 'H11_A_ATCACG':
+                if processed_file == 'H11_A_ATCACG': # if the first file is processed we want names of sequences in first column
                     result_to_write = str(df.iloc[i, 2]) + ' ' + str(count) + '\n'
                 else:
                     result_to_write = str(count) + '\n'
