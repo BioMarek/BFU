2a56443ea2f0c2ba21ae806d32cffeacc0b64733
BioMarek <BioMarek@users.noreply.github.com>
2017-05-09 12:30:58 +0200

    Update 4_count_bowtie_linux.sh

--- Rfam_database_mining/4_count_bowtie_linux.sh
+++ Rfam_database_mining/4_count_bowtie_linux.sh
@@ -26,13 +26,14 @@ python $PROJECT_DIR/scripts/4_count_bowtie.py
 mv *.counts $OUTPUT_DIR
 cd $OUTPUT_DIR
 
-# creates matrix file with header
-touch ncRNA_matrix_count.counts doesn't work use sed
-echo 'name\t H11_A \tH11_B \tP1_A \tP1_B \tP3_A \tP3_B \tP8_A \tP8_B \tREG_A \tREG_B' > ncRNA_matrix_count.counts
-
 # creates matrix file
 paste H11_A_ATCACG_aligned.counts H11_B_CGATGT_aligned.counts P1_A_CAGATC__aligned.counts P1_B_ACTTGA__aligned.counts \
       P3_A_GATCAG__aligned.counts P3_B_TAGCTT__aligned.counts P8_A_TTAGGC__aligned.counts P8_B_TGACCA__aligned.counts \
-      REG_A_ACAGTG_aligned.counts REG_B_GCCAAT_aligned.counts >> ncRNA_matrix_count.counts
+      REG_A_ACAGTG_aligned.counts REG_B_GCCAAT_aligned.counts > ncRNA_matrix_count.counts
+
+# add header, we have to go through temporary file because we cannot modify file and at the same time redirect 
+# output into it
+sed '1 i\name\t H11_A \tH11_B \tP1_A \tP1_B \tP3_A \tP3_B \tP8_A \tP8_B \tREG_A \tREG_B' ncRNA_matrix_count.counts > temp
+mv temp ncRNA_matrix_count.counts
       
 rm *aligned.counts
