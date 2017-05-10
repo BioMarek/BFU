42301a9a405bc7f7960073e17afe836c008a6ba7
BioMarek <BioMarek@users.noreply.github.com>
2017-05-10 13:03:34 +0200

    Update 4_count_matrix_miRNAs.sh

--- 4_count_matrix_miRNAs.sh
+++ 4_count_matrix_miRNAs.sh
@@ -1,5 +1,5 @@
-#TODO test it
-# error in python script with comparison which is not in python script used in rfam branch
+# TODO test it
+# error in python script remove duplicates
 #! /bin/bash
 #
 #######################################################################################################################
