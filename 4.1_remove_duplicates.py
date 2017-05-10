cca833e58725fd8106c52f184aff912aaacf2546
BioMarek <BioMarek@users.noreply.github.com>
2017-05-10 13:48:20 +0200

    Update 4.1_remove_duplicates.py

--- 4.1_remove_duplicates.py
+++ 4.1_remove_duplicates.py
@@ -4,11 +4,11 @@ from Bio import SeqIO
 f_result = open('Nicotiana_miRNA_D_no_duplicates.fa', 'w')
 
 sequences = []
-for seq_record in SeqIO.parse('Nicotiana_miRNA_D.fa', "fasta"):
+for seq_record in SeqIO.parse('Nicotiana_miRNA_D.fa', 'fasta'):
     # print(seq_record.seq)
     if seq_record.seq not in sequences:
         sequences.append(seq_record.seq)
-        final_result = (">" +seq_record.description + '\n' + seq_record.seq + '\n')
+        final_result = ('>' +seq_record.description + '\n' + seq_record.seq + '\n')
 
 # print(len(sequences))
 f_result.close()
