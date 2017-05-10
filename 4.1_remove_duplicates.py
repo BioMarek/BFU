2a43ccfe908ea0af3e5231c72f35993623924829
BioMarek <BioMarek@users.noreply.github.com>
2017-05-10 09:41:30 +0200

    Update 4.1_remove_duplicates.py

--- 4.1_remove_duplicates.py
+++ 4.1_remove_duplicates.py
@@ -1,10 +1,10 @@
 from Bio import SeqIO
 
 # The script removes duplicates from fasta file
-f_result = open("Nicotiana_Rfam_seq_no_duplicates.fa", 'w')
+f_result = open("Nicotiana_miRNA_D.fa", 'w')
 
 sequences = []
-for seq_record in SeqIO.parse("Nicotiana_Rfam_seq.fa", "fasta"):
+for seq_record in SeqIO.parse("Nicotiana_miRNA_D_no_duplicates.fa", "fasta"):
     print(seq_record.seq)
     if seq_record.seq not in sequences:
         sequences.append(seq_record.seq)
