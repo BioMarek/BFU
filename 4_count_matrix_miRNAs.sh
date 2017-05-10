680c2a6a0ce129184223b8598138d2dfc3266010
BioMarek <BioMarek@users.noreply.github.com>
2017-05-10 09:02:03 +0200

    Update 4_count_matrix_miRNAs.sh

--- 4_count_matrix_miRNAs.sh
+++ 4_count_matrix_miRNAs.sh
@@ -31,10 +31,10 @@ cd $OUTPUT_DIR
 paste H11_A_ATCACG_temp.counts H11_B_CGATGT_temp.counts P1_A_CAGATC__temp.counts P1_B_ACTTGA__temp.counts \
       P3_A_GATCAG__temp.counts P3_B_TAGCTT__temp.counts P8_A_TTAGGC__temp.counts P8_B_TGACCA__temp.counts \
       REG_A_ACAGTG_temp.counts REG_B_GCCAAT_temp.counts > counts_all.counts
-      
+
 # add header, we have to go through temporary file because we cannot modify file and at the same time redirect 
 # output into it
 sed '1 i\name\t H11_A \tH11_B \tP1_A \tP1_B \tP3_A \tP3_B \tP8_A \tP8_B \tREG_A \tREG_B' counts_all.counts > temp
 mv temp ncRNA_matrix_count.counts
-      
+
 rm *_temp.counts
