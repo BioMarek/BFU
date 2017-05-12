1bcab775cb0b3a77333dc5407bc41b7d10b50db7
BioMarek <BioMarek@users.noreply.github.com>
2017-05-12 09:05:05 +0200

    Create 3.3_start_nucleotide_count.sh

---
+++ 3.3_start_nucleotide_count.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+## SCRIPT INPUT  *.fastq.gz filtered sequences with proper length and high quality
+## SCRIPT OUTPUT *.txt file counts with length distribution and starting nucleotide distribution
+# The script counts length distribution and starting nucleotide distribution of sequences so we can make histogram
+
+#######################################################################################################################
+##SPECIFY DATA VARIABLES###
+PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
+DATASET_DIR=$PROJECT_DIR/filtered # path to input filtered sequences
+OUTPUT_DIR=$PROJECT_DIR/start_counts # path to output sequences
+
+#######################################################################################################################
+###SCRIPT BODY###
+mkdir $OUTPUT_DIR
+cd $DATASET_DIR
+
+for file in *mirna.fastq.gz
+do
+
+done
