41808b6113a50d6d5d91cf2005c18af85e56a07b
BioMarek <BioMarek@users.noreply.github.com>
2017-05-16 11:34:11 +0200

    Update 4_count_bowtie.py

--- Rfam_database_mining/4_count_bowtie.py
+++ Rfam_database_mining/4_count_bowtie.py
@@ -1,43 +1,46 @@
-# TODO it doesn't work!!
-
-#######################################################################################################################
-###INFORMATION ABOUT THE SCRIPT###
-## SCRIPT INPUT  *.bow files from bowtie
-## SCRIPT OUTPUT *.counts files
-# The script takes alignment files from bowtie and creates counts for each file. These files are in subsequent step 
-# merged to create count matrix.
-
-#######################################################################################################################
-###SCRIPT BODY###
 import pandas
 
-processed_list = ['H11_A_ATCACG', 'H11_B_CGATGT','P1_A_CAGATC_', 'P1_B_ACTTGA_', 'P3_A_GATCAG_', 'P3_B_TAGCTT_',
+processed_list = ['H11_A_ATCACG', 'H11_B_CGATGT', 'P1_A_CAGATC_', 'P1_B_ACTTGA_', 'P3_A_GATCAG_', 'P3_B_TAGCTT_',
                   'P8_A_TTAGGC_', 'P8_B_TGACCA_', 'REG_A_ACAGTG', 'REG_B_GCCAAT']
 
+# creates reference list with names of sequences
+sequences = []
 for processed_file in processed_list:
-    f_result = open(processed_file + '_aligned.counts', 'w')
-    sequences = []
-    count = 0
-
     input_file = open(processed_file + '_aligned.bow', 'r')
-
     df = pandas.read_csv(input_file, delimiter='\t')
 
     for i in range(len(df)):
-        # deals with problem with first zero
-        if i == 0:
-            # iloc takes specific field in matrix
+        if df.iloc[i, 2] not in sequences:
             sequences.append(df.iloc[i, 2])
-            count += int(df.iloc[i, 0])
-        else:
-            if df.iloc[i, 2] not in sequences:
-                sequences.append(df.iloc[i, 2])
-                if processed_file == 'H11_A_ATCACG': # if the first file is processed we want names of sequences in first column
-                    result_to_write = str(df.iloc[i, 2]) + ' ' + str(count) + '\n'
-                else:
-                    result_to_write = str(count) + '\n'
-                f_result.write(result_to_write)
-                count = 0
-            else:
+
+    input_file.close()
+
+###WHEN USING LINUX SORTED FILES PANDAS THROEUWS WARNING.
+# creates count matrix for each file
+# goes through each file
+for processed_file in processed_list:
+    f_result = open(processed_file + '_aligned.counts', 'w')
+    input_file = open(processed_file + '_aligned.bow', 'r')
+    df = pandas.read_csv(input_file, delimiter='\t')
+
+    # goes through each sequence in reference list
+    for seq in range(len(sequences)):
+        count = 0
+        # goes through each line in processed_file and compares with analyzed sequence
+        flag = 0  # flag that tells us that we are currently in block of sequences that are being counted
+        for i in range(len(df)):
+            if df.iloc[i, 2] == sequences[seq]:
+                flag = 1
                 count += int(df.iloc[i, 0])
+            if (df.iloc[i, 2] != sequences[seq]) and flag == 1:
+                break
+
+        if processed_file == 'H11_A_ATCACG':
+            result_to_write = sequences[seq] + ' ' + str(count) + '\n'
+        else:
+            result_to_write = str(count) + '\n'
+
+        f_result.write(result_to_write)
+
+    input_file.close()
     f_result.close()
