9abae8c5c6c11158365ed0a6dac8bff4fcbdd176
BioMarek <BioMarek@users.noreply.github.com>
2017-05-17 16:19:29 +0200

    Update 4_count_bowtie.sh

--- Rfam_database_mining/4_count_bowtie.sh
+++ Rfam_database_mining/4_count_bowtie.sh
@@ -33,14 +33,6 @@ module add python26-modules-intel
 cp $PROJECT_DIR/scripts/4_count_bowtie.py $SCRATCH
 python 4_count_bowtie.py $1
 
-# creates matrix file
-paste H11_A_ATCACG_aligned.counts H11_B_CGATGT_aligned.counts P1_A_CAGATC__aligned.counts P1_B_ACTTGA__aligned.counts \
-      P3_A_GATCAG__aligned.counts P3_B_TAGCTT__aligned.counts P8_A_TTAGGC__aligned.counts P8_B_TGACCA__aligned.counts \
-      REG_A_ACAGTG_aligned.counts REG_B_GCCAAT_aligned.counts > ncRNA_matrix_count.counts
-
-# add header, we have to go through temporary file because we cannot modify file and at the same time redirect 
-# output into it
-sed '1 i\name\t H11_A \tH11_B \tP1_A \tP1_B \tP3_A \tP3_B \tP8_A \tP8_B \tREG_A \tREG_B' ncRNA_matrix_count.counts > temp
-mv temp $OUTPUT_DIR/ncRNA_matrix_count.counts
+cp *.counts $OUTPUT_DIR
 
 rm *
