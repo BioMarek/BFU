d3c39e28bdab0b44bd798e9328609a226b99afc1
BioMarek <BioMarek@users.noreply.github.com>
2017-05-24 12:07:13 +0200

    Update 1_trimming.sh

--- 1_trimming.sh
+++ 1_trimming.sh
@@ -37,3 +37,8 @@ do
 done
 
 rm -rf *
+
+# in subsequent steps different python modules are required, when two version of python module are loaded scripts
+# dont't work as intended
+module unload python27-modules-gcc
+module unload python27-modules-intel
