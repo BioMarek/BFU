96c18ce88ee2a2c45c94ee5c24da22872e85a5cc
BioMarek <BioMarek@users.noreply.github.com>
2017-05-24 08:14:51 +0200

    Update 0_main_miRNA.sh

--- 0_main_miRNA.sh
+++ 0_main_miRNA.sh
@@ -3,7 +3,8 @@
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
 # This script runs the pipeline
-# Start with: qsub -l walltime=12h -l mem=1gb -l scratch=40gb -l nodes=1:ppn=2 0_main.sh
+# Start with: qsub -l walltime=4:0:0 -q default -l select=1:ncpus=4:mem=4gb:scratch_local=40gb -I
+# Stop with:  qdel 
 PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA
 cd $PROJECT_DIR
 
