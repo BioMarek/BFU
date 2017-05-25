dc1bf0e837fd19989d85c90d43bb17d06a43c84b
BioMarek <BioMarek@users.noreply.github.com>
2017-05-25 10:23:58 +0200

    Update 1_counting_nptII.sh

--- nptII_analysis/1_counting_nptII.sh
+++ nptII_analysis/1_counting_nptII.sh
@@ -19,7 +19,7 @@ cd $DATASET_DIR
 for file in *.fasta
 do
    # leaves sequences from fastq file, removes everything else, awk prints lengths of sequencs; uniq counts the sequences.
-   sed -n 'n;p;' | awk '{print length}' | sort | uniq -c > $OUTPUT_DIR/${file:0:4}_counts.txt
+   sed -n 'n;p;' $file | awk '{print length}' | sort | uniq -c > $OUTPUT_DIR/${file:0:3}_counts.txt
    # chmod sets access right so that only owner can work with file
-   chmod 700 $OUTPUT_DIR/${file:0:4}_counts.txt
+   chmod 700 $OUTPUT_DIR/${file:0:3}_counts.txt
 done
