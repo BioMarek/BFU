80f3ef9a5505e916518803952ed10966754b3e38
BioMarek <BioMarek@users.noreply.github.com>
2017-05-25 09:47:08 +0200

    Update and rename length_counting.sh to 1_counting_nptII.sh

---
+++ nptII_analysis/1_counting_nptII.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+#
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+## SCRIPT INPUT  *.fasta reads mapped to nptII gene from geneious
+## SCRIPT OUTPUT *.txt files with length counts
+
+#######################################################################################################################
+##SPECIFY DATA VARIABLES###
+PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
+DATASET_DIR=$PROJECT_DIR/nptII/mapped_reads # path to input filtered sequences
+OUTPUT_DIR=$PROJECT_DIR/nptII/counts # path to output sequences
+
+#######################################################################################################################
+###SCRIPT BODY###
+mkdir -p $OUTPUT_DIR
+cd $DATASET_DIR
+
+for file in *.fasta
+do
+   # leaves sequences from fastq file, removes everything else, awk prints lengths of sequencs; uniq counts the sequences.
+   sed -n 'n;p;' | awk '{print length}' | sort | uniq -c > $OUTPUT_DIR/${file:0:4}_counts.txt
+   # chmod sets access right so that only owner can work with file
+   chmod 700 $OUTPUT_DIR/${file:0:4}_counts.txt
+done
