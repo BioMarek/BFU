b8cd154eb32dc6e8023de0923c2277a74cf6fee1
BioMarek <BioMarek@users.noreply.github.com>
2017-05-26 16:26:02 +0200

    Update _3.3_start_nucleotide_count2.sh

--- _3.3_start_nucleotide_count2.sh
+++ _3.3_start_nucleotide_count2.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 #
 # http://stackoverflow.com/questions/11006431/sort-by-third-column-leaving-first-and-second-column-intact-in-linux
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+## SCRIPT INPUT  *.fasta files mapped to nptII gene
+## SCRIPT OUTPUT *.txt 
+# The script counts length and nuclieotide distribution of sequences so we can make graph
 
-for line in `cat H11_mapped.fasta | sed -n 'n;p;'
-do 
-  echo ${#line}$'\t'${line} 
-done | sort -n | cut -f 2- > res
+#######################################################################################################################
+##SPECIFY DATA VARIABLES###
+PROJECT_DIR=/storage/brno7-cerit/home/marek_bfu/smRNA # path to project dir
+DATASET_DIR=$PROJECT_DIR/nptII/mapped_reads # path to input filtered sequences
+OUTPUT_DIR=$PROJECT_DIR/nptII/first_nucleotide # path to output sequences
+
+#######################################################################################################################
+###SCRIPT BODY###
+# takes each line in file, keeps only sequence and removes everything else
+mkdir $OUTPUT_DIR
+cd $DATASET_DIR
+
+for file in *.fasta
+do
+  for line in `cat $file | sed -n 'n;p;'`
+  do
+    echo ${#line}$'\t'${line}
+  done | sort -n | cut -f 2- > ${file:0:3}_sorted.txt
+done
