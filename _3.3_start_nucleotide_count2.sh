95365b6f37fdd1ee774a2093f9c63225d6f35e35
BioMarek <BioMarek@users.noreply.github.com>
2017-05-26 16:09:19 +0200

    Update _3.3_start_nucleotide_count2.sh

--- _3.3_start_nucleotide_count2.sh
+++ _3.3_start_nucleotide_count2.sh
@@ -2,7 +2,7 @@
 #
 # http://stackoverflow.com/questions/11006431/sort-by-third-column-leaving-first-and-second-column-intact-in-linux
 
-for i in `cat your_file`
+for line in `cat H11_mapped.fasta | sed -n 'n;p;'
 do 
-  echo ${#i}$'\t'${i} 
+  echo ${#line}$'\t'${line} 
 done | sort -n | cut -f 2- > res
