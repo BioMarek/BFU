0ba21199ec3da9bce54705c6285038fad0fa1691
BioMarek <BioMarek@users.noreply.github.com>
2017-10-23 09:54:33 +0200

    Update 0_collapsed_to_csv.sh

--- statistic/0_collapsed_to_csv.sh
+++ statistic/0_collapsed_to_csv.sh
@@ -3,8 +3,8 @@
 #######################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
 ## SCRIPT INPUT  3_collapsing_miRNA.sh
-## SCRIPT OUTPUT *.csv file
-# The script creates *.csv file from collapsed files
+## SCRIPT OUTPUT *.csv files
+# The script creates *.csv file from collapsed files for further analysis with python
 
 #######################################################################################################################
 ##SPECIFY DATA VARIABLES##
@@ -20,7 +20,16 @@ mkdir -p $OUTPUT_DIR
 # multiple file solution
 for file in *.txt
 do
-  # switches the columns, the ',' is delimiter
+  # switches the columns, the ',' is delimiter and adds first column containing name of sample
   awk '{ print "'"${file:0:5}"'" "," $2 "," $1 }' $file | sort -k2 > ${file:0:12}.csv
   mv ${file:0:12}.csv $OUTPUT_DIR
 done
+
+# creates single file with header for pandas analysis
+cd $OUTPUT_DIR
+cat * > collapsed_smRNA.csv
+
+# add header, we have to go through temporary file because we cannot modify file and at the same time redirect 
+# output into it
+sed '1 i\sample, sequence, count collapsed_smRNA.csv> temp
+mv temp collapsed_smRNA.csv
