a00de632cd31132e269d29952a436a28c2f989ec
BioMarek <BioMarek@users.noreply.github.com>
2017-10-25 11:40:53 +0200

    Update 1_statistics_pandas.py

--- statistic/1_statistics_pandas.py
+++ statistic/1_statistics_pandas.py
@@ -4,34 +4,32 @@ import pandas as pd
 def basic_statistics(df):
     missing_in_sample = 0  # sequences that are missing in one of then samples
     in_all_samples = 0  # sequences that were found in all samples
-    sequence_count = 0
-    sequence_stored = ''  # stores currently counted sequence
-    for row in range(len(df)):
+    sequence_count = 1
+    sequence_stored = df.iloc[0, 1]  # stores first sequence
 
-        if sequence_count == 0:
-            sequence_stored = df.iloc[row, 1]
-            sequence_count = 1
-            continue
-
+    for row in range(1, len(df)):
+        print('row:', row, 'seq tested:', df.iloc[row, 1], 'seq stored:', sequence_stored, 'seq count:', sequence_count,
+              'ic count:', missing_in_sample, 'c count', in_all_samples)  # TODO
         if sequence_stored == df.iloc[row, 1]:
             sequence_count = sequence_count + 1
-        else:
             if sequence_count == 10:
                 in_all_samples = in_all_samples + 1
-            else:
-                missing_in_sample = missing_in_sample + 1
-            sequence_count = 0
+                sequence_count = 1
+        else:
+            missing_in_sample = missing_in_sample + 1
+            sequence_count = 1
+            sequence_stored = df.iloc[row, 1]
             continue
 
     return missing_in_sample, in_all_samples, len(df)
 
 
-df = pd.read_csv('collapsed_smRNA.csv')
+df = pd.read_csv('test.csv')
 print(df.head(n=30))
-df.sort_values(by='sequence', inplace=True)
-print(df.head(n=30))
-print(df.shape)
-print(df.info())
+#df.sort_values(by='sequence', inplace=True)
+#print(df.head(n=30))
+print('\n', df.shape)
+print('\n', df.info())
 a, b, c = basic_statistics(df)
-print('missing is: ', a, str(a/c), '%, complete is: ', b, str(b/c), '%.')
+print('\nmissing is: ', a, str(a / c * 100), '%, complete is: ', b, str(b * 10 / c * 100), '%, length is: ', str(c))
 
