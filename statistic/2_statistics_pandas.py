2fc4aefa5ac7e24cbbf5d7fa5c6ee7b84cc17fa4
BioMarek <BioMarek@users.noreply.github.com>
2018-01-18 09:36:11 +0100

    Update 2_statistics_pandas.py

--- statistic/2_statistics_pandas.py
+++ statistic/2_statistics_pandas.py
@@ -4,8 +4,8 @@ import pandas as pd
 class BasicStatistics:
     def __init__(self):
         self.count_list = []
-        self.name_list = ['H11_A', 'H11_B', 'P1_A_', 'P1_B_', 'P3_A_', 'P3_B_', 'P8_A_', 'P8_B_', 'REG_A', 'REG_B']  # TODO make dictionary
-        self.sequence_stored = ''
+        self.name_list = ['H11_A', 'H11_B', 'P1_A_', 'P1_B_', 'P3_A_', 'P3_B_', 'P8_A_', 'P8_B_', 'REG_A', 'REG_B']
+        self.sequence_stored = ''  # sequence which is currently being tested
         self.unique_count = 0  # counts number of unique sequences
         self.size = 5  # number of biological replicates
         self.pattern_match_result = [0 for x in range(2 ** self.size)]  # generates list to store results from pattern matching
@@ -17,7 +17,6 @@ class BasicStatistics:
         because we analyze technical duplicates.
         :return: list of lists containing combinations.
         """
-        # TODO remove all false first combination
         binary_set = [0 for x in range(self.size)]
         final_set = []
         
@@ -52,15 +51,13 @@ class BasicStatistics:
             calculate(binary_set)
             
         self.set_of_patterns = final_set  # saves pattern set so we can use in result presentation
-        return final_set
 
     def match_to_pattern(self, pattern_list):
-        """"The function counts number of unique sequences that occur only in regenerated and parental plants"""
+        """"The function """
         for pattern_row in range(len(pattern_list)):
             if self.count_list == pattern_list[pattern_row]:
                 self.pattern_match_result[pattern_row] += 1
                 break
-                # TODO addition to pattern result storage
 
     def test_name_list(self, row):
         """The function saves information about name of sample in currently tested row"""
@@ -71,6 +68,8 @@ class BasicStatistics:
 
     def count(self, df):
         """"The function goes through dataframe and counts occurrence of sample for each sequence"""
+        self.pattern_generator()
+        
         for row in range(len(df)):  # goes through each row in dataframe
 
             if row % 10000 == 0:  # TODO
@@ -82,7 +81,7 @@ class BasicStatistics:
             else:
                 # print(self.count_list, self.sequence_stored)  # TODO
                 self.unique_count += 1
-                self.match_to_pattern(self.pattern_generator())
+                self.match_to_pattern(self.set_of_patterns)
 
                 self.count_list = [0 for i in range(10)]  # list reset, when row = 0 it is also initialization
                 self.sequence_stored = df.iloc[row, 1]  # sets currently tested sequence
