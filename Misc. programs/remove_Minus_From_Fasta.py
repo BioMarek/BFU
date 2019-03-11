c2de8c1ad5a21dbefcd7bad7fe45cc3b6bb68f3d
BioMarek <BioMarek@users.noreply.github.com>
2019-03-11 13:52:43 +0100

    Update remove_Minus_From_Fasta.py

--- Misc. programs/remove_Minus_From_Fasta.py
+++ Misc. programs/remove_Minus_From_Fasta.py
@@ -1,6 +1,6 @@
 # program removes all "-" from given file
 
-with open('results/*.fasta', 'r') as inputFile, open('results/*.fasta bez pomlcek.fasta', 'w') as outputFile:
+with open('*.fasta', 'r') as inputFile, open('*.fasta bez pomlcek.fasta', 'w') as outputFile:
     data = inputFile.read()
     data = data.replace("-", "")
     outputFile.write(data)
