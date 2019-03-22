61f0ec9eaa8323cbc868748e22f8604c83bf54ae
BioMarek <BioMarek@users.noreply.github.com>
2019-03-22 13:00:53 +0100

    Update bending_calculation.py

--- Misc. programs/BendingCalculations/bending_calculation.py
+++ Misc. programs/BendingCalculations/bending_calculation.py
@@ -15,15 +15,20 @@ with open('_test.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     reader = csv.reader(csv_file)
 
-    phosphateRows =[]
+    phosphateRows = []
+    next(reader)
     for row in reader:
-        phosphateRows.append(row[8:14])
-        print(row[0], row[8:14])
-        #print(row)
+        phosphateRows.append([float(row[8]), float(row[9]), float(row[10])])  # appends only phosphate1 coordinates
 
-phosphateRows = phosphateRows[1:]
-print(phosphateRows)
+result = []
+windowSize = 20
+if len(phosphateRows) > windowSize:
+    i = 0
+    while len(phosphateRows) > windowSize + i:
+        result.append(calculateDistance(phosphateRows[i], phosphateRows[i + windowSize]))
+        #print(calculateDistance(phosphateRows[i], phosphateRows[i + windowSize]))
+        i += 1
 
-print(calculateDistance([-69.36834, 9.17868, -24.17384], [-71.78425, 7.23292, -19.04525]))
+print(result)
 
 
