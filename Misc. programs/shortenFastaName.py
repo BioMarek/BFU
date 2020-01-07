a68e3091453f53b609c17e942d2790ccf85899d9
BioMarek <BioMarek@users.noreply.github.com>
2020-01-07 14:44:03 +0100

    Create shortenFastaName.py

---
+++ Misc. programs/shortenFastaName.py
@@ -0,0 +1,21 @@
+inputPath = 'c:\Programing\Python\sequences.fasta'
+outputPath = 'c:\Programing\Python\sequencesR.fasta'
+
+prefixToDelete = 'A00721:127:HW5KYDSXX:4:'
+suffixToDelete = '_1:N:0:GGTCTGGT+AAGGCCGT'
+
+with open(inputPath, 'r') as inputFile, open(outputPath, 'w') as outputFile:
+    line = inputFile.readline()
+    while line:
+        if line[0] == '>':
+            leftIndex = line.find(prefixToDelete) + len(prefixToDelete)
+            rightIndex = len(line) - line.find(suffixToDelete)
+
+            if leftIndex > 0:
+                line = line[leftIndex:]
+            if rightIndex > 0:
+                line = line[:-rightIndex]
+            line = '>' + line + '\n'
+
+        outputFile.write(line)
+        line = inputFile.readline()
