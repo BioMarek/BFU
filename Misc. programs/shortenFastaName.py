705f4270665bcc6db068495260f5a36c0459c23d
BioMarek <BioMarek@users.noreply.github.com>
2020-02-10 16:26:49 +0100

    Update shortenFastaName.py

--- Misc. programs/shortenFastaName.py
+++ Misc. programs/shortenFastaName.py
@@ -7,13 +7,14 @@ suffixToDelete = '_1:N:0:GGTCTGGT+AAGGCCGT'
 with open(inputPath, 'r') as inputFile, open(outputPath, 'w') as outputFile:
     line = inputFile.readline()
     while line:
+
         if line[0] == '>':
             leftIndex = line.find(prefixToDelete) + len(prefixToDelete)
             rightIndex = len(line) - line.find(suffixToDelete)
 
-            if leftIndex > 0:
+            if leftIndex > 0 and len(prefixToDelete) > 0:
                 line = line[leftIndex:]
-            if rightIndex > 0:
+            if rightIndex > 0 and len(suffixToDelete) > 0:
                 line = line[:-rightIndex]
             line = '>' + line + '\n'
 
